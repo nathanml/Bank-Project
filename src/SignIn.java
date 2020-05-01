493c35999a9f3d3b777993784fcb9eed71087753
kelsey-myton <48225178+kelsey-myton@users.noreply.github.com>
2020-05-01 19:59:02 -0400

    Edited Sign up and Sign In

--- src/SignIn.java
+++ src/SignIn.java
@@ -1,11 +1,96 @@
-public class SignIn {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.*;
     /*
-    * Sign in for all users-bank manager and customer
+    *  Class for signing in a user
     * */
-
-    public static void main(String[] args)
+public class SignIn extends JFrame implements ActionListener
+{
+	//Swing things
+	private static JPanel panel;
+	private static JLabel welcome;
+	private static JLabel userlabel;
+	private static JLabel passlabel;
+	private static JTextField userText;
+	private static JPasswordField passText;
+	private static JButton button;
+	
+    // No-arg constructor to construct an instance of JFrame
+    public SignIn () 
     {
-
     }
+    
+    public void initializeframe() {
+
+        setTitle( "Sign In" );
+        setSize( 500, 500 );
+        setLocation( 400, 100 );
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        
+        //create a panel
+        panel= new JPanel();
+        add(panel);
+        panel.setLayout(null);
+        
+        //Add welcome text
+        welcome= new JLabel("Welcome to our Bank, Please sign in!");
+        panel.add(welcome);
+        
+        
+        //Create label and textbox for username
+        userlabel= new JLabel("Username:");
+        userlabel.setBounds(10, 20, 80, 25);
+        panel.add(userlabel);
+        
+        userText= new JTextField();
+        userText.setBounds(100,20,165,25);
+        panel.add(userText);
+        
+        //Create label and textbox for password
+        passlabel= new JLabel("Password:");
+        passlabel.setBounds(10, 50, 80, 25);
+        panel.add(passlabel);
+        
+        passText= new JPasswordField();
+        passText.setBounds(100, 50, 165, 25);
+        panel.add(passText);
+     
+        //Create button
+        button= new JButton("Sign up");
+        button.setBounds(10, 80, 80, 25);
+        panel.add(button);
+        button.addActionListener(new SignIn()); 
+        
+        setVisible( true );
+    }
+    
+    //just for testing purposes
+    public static void main( String[] args ) 
+    {
+    	SignIn si= new SignIn();
+    	si.initializeframe();
+    }
+    
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String username= userText.getText();
+		String password= passText.getText();
+		
+		//check if user exists in database already
+		if (!username.equals("") && !password.equals(""))
+		{
+			//make customer object
+			Customer c1= new Customer();
+			System.out.println("You have successfully signed in!");
+			
+			//alert listener
+			//give customer to ATM
+			
+		}
+		else {
+		System.out.println("Incorrect input, please try again");
+		}
+		
+	}
 }
--- src/SignUp.java
+++ src/SignUp.java
@@ -8,88 +8,107 @@ import javax.swing.*;
 public class SignUp extends JFrame implements ActionListener
 {
 	//Swing things
-	private static JFrame frame;
 	private static JPanel panel;
 	private static JLabel welcome;
 	private static JLabel userlabel;
 	private static JLabel passlabel;
+	private static JLabel fnamelabel;
+	private static JLabel lnamelabel;
 	private static JTextField userText;
 	private static JPasswordField passText;
+	private static JTextField fnameText;
+	private static JTextField lnameText;
 	private static JButton button;
-	private static JLabel success;
 	
-    // No-arg constructor to construct an instance o JFrame
+    // No-arg constructor to construct an instance of JFrame
     public SignUp () 
     {
-    	//This stuff isnt used, I did it all in main. will change later
-	    setTitle( "Sign" );
-	    setSize( 500, 500 );
-	    setLocation( 400, 100 );
-	    setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-	    setVisible( true );
     }
- 
+    
+    public void initializeframe() {
+
+        setTitle( "Sign up" );
+        setSize( 500, 500 );
+        setLocation( 400, 100 );
+        setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        
+        //create a panel
+        panel= new JPanel();
+        add(panel);
+        panel.setLayout(null);
+        
+        //Add welcome text
+        welcome= new JLabel("Welcome to our Bank, Please sign up!");
+        panel.add(welcome);
+        
+        //Create label and textbox for first name
+        fnamelabel= new JLabel("First Name:");
+        fnamelabel.setBounds(10, 20, 80, 25);
+        panel.add(fnamelabel);
+        
+        fnameText= new JTextField();
+        fnameText.setBounds(100, 20, 165, 25);
+        panel.add(fnameText);
+        
+      //Create label and textbox for last name
+        lnamelabel= new JLabel("Last Name:");
+        lnamelabel.setBounds(10, 50, 80, 25);
+        panel.add(lnamelabel);
+        
+        lnameText= new JTextField();
+        lnameText.setBounds(100, 50, 165, 25);
+        panel.add(lnameText);
+        
+        //Create label and textbox for username
+        userlabel= new JLabel("Username:");
+        userlabel.setBounds(10, 80, 80, 25);
+        panel.add(userlabel);
+        
+        userText= new JTextField();
+        userText.setBounds(100,80,165,25);
+        panel.add(userText);
+        
+        //Create label and textbox for password
+        passlabel= new JLabel("Password:");
+        passlabel.setBounds(10, 110, 80, 25);
+        panel.add(passlabel);
+        
+        passText= new JPasswordField();
+        passText.setBounds(100, 110, 165, 25);
+        panel.add(passText);
+     
+        //Create button
+        button= new JButton("Sign up");
+        button.setBounds(10, 140, 80, 25);
+        panel.add(button);
+        button.addActionListener(new SignUp()); 
+        
+        setVisible( true );
+    }
+    
+    //just for testing purposes
     public static void main( String[] args ) 
     {
-    // Create an instance of TestFrame
-    //SignUp signupframe = new SignUp();
-    // Create an instance of JFrame() explicitly
-    frame = new JFrame();
-    frame.setTitle( "Sign up" );
-    frame.setSize( 500, 500 );
-    frame.setLocation( 400, 100 );
-    frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-    
-    //create a panel
-    panel= new JPanel();
-    frame.add(panel);
-    panel.setLayout(null);
-    
-    //Add welcome text
-    welcome= new JLabel("Welcome to our Bank, Please sign up!");
-    panel.add(welcome);
-    
-    //Create label and textbox for username
-    userlabel= new JLabel("Username:");
-    userlabel.setBounds(10, 20, 80, 25);
-    panel.add(userlabel);
-    
-    userText= new JTextField();
-    userText.setBounds(100,20,165,25);
-    panel.add(userText);
-    
-    //Create label and textbox for password
-    passlabel= new JLabel("Password:");
-    passlabel.setBounds(10, 50, 80, 25);
-    panel.add(passlabel);
-    
-    passText= new JPasswordField();
-    passText.setBounds(100, 50, 165, 25);
-    panel.add(passText);
-    
-    //Create button
-    button= new JButton("Sign up");
-    button.setBounds(10, 80, 80, 25);
-    panel.add(button);
-    button.addActionListener(new SignUp());
-    
-    //Check for success
-    success= new JLabel("");
-    success.setBounds(10, 110, 300, 25);
-    panel.add(success);
-    //success.setText(text);
-    
-    
-    frame.setVisible( true );
+    	SignUp si= new SignUp();
+    	si.initializeframe();
+    	
     }
     
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		String firstname= fnameText.getText();
+		String lastname= lnameText.getText();
 		String username= userText.getText();
 		String password= passText.getText();
+		
+		//check if user exists in database already
 		if (!username.equals("") && !password.equals(""))
 		{
-			Customer c1= new Customer(username, password, username, password);
+			//add user to database
+			Customer c1= new Customer(firstname, lastname, username, password);
+			System.out.println("You have successfully signed up!");
+			
+			//alert listener
 		}
 		else {
 		System.out.println("Incorrect input, please try again");
