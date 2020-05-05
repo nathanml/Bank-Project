11d250e605888bd080b8ff41c7620f3ac3620f96
mzheng27 <59420248+mzheng27@users.noreply.github.com>
2020-05-05 12:38:15 -0400

    Delete OPObserver.java

--- OPObserver.java
+++
@@ -1,9 +0,0 @@
-/*
-* OPObserver - Interface for the observer in the Observer Pattern
-*/
-public interface OPObserver {
-	 String update1();
-	 void update2();
-	 boolean equals(OPObserver other);
-	 String getName();
-}
